07fb653bfa906a31ca4b04c0d49e674a110475f2
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-10-17 21:11:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("Enter the character:",end = "")
+c = input()
+if len(c)>1:
+    print("\nInvalid input")
+else:
+    if c>='a' and c<='z':
+        print("\n\""+c+"\"is an alphabet.")
+    elif c>='A' and c<='Z':
+        print("\n\""+c+"\"is an alphabet.")
+    else:
+        print("\n\""+c+"\"is not an alphabet.")
